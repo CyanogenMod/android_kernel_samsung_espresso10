11fad658d36b91f17e5ec85060784a1f659707b9
Daniel Hillenbrand <daniel.hillenbrand@codeworkx.de>
2012-07-29 13:17:25 +0200

    bcmdhd: fix some p2p errors

--- drivers/net/wireless/bcmdhd/src/dhd/sys/dhd_linux.c
+++ drivers/net/wireless/bcmdhd/src/dhd/sys/dhd_linux.c
@@ -3635,9 +3635,11 @@ dhd_preinit_ioctls(dhd_pub_t *dhd)
 #if defined(SOFTAP)
 	if (ap_fw_loaded == FALSE)
 #endif
+	if ((dhd->op_mode & HOSTAPD_MASK) != HOSTAPD_MASK) {
 		if ((res = dhd_keep_alive_onoff(dhd)) < 0)
 			DHD_ERROR(("%s set keeplive failed %d\n",
 			__FUNCTION__, res));
+		}
 	}
 #endif /* defined(KEEP_ALIVE) */
 
