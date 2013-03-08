9b93542ff951c1dc1db0f6cc9dd34f418eb05aed
Ketut Putu Kumajaya <kumajaya@samator.com>
2013-03-08 14:18:24 +0700

    stop the spam

--- drivers/misc/modem_if_v2/modem_link_device_mipi.c
+++ drivers/misc/modem_if_v2/modem_link_device_mipi.c
@@ -30,6 +30,7 @@
 #include "modem_link_device_mipi.h"
 #include "modem_utils.h"
 
+#define DEBUG_PRINT 0
 
 static int mipi_hsi_init_communication(struct link_device *ld,
 			struct io_device *iod)
@@ -231,6 +232,7 @@ static void mipi_hsi_tx_work(struct work_struct *work)
 			}  else {
 				mipi_debug("write Done\n");
 
+#if DEBUG_PRINT
 				if ((iod->format == IPC_FMT) ||
 						(iod->format == IPC_RFS))
 					print_hex_dump(KERN_DEBUG,
@@ -242,6 +244,7 @@ static void mipi_hsi_tx_work(struct work_struct *work)
 							fmt_skb->len <= 16 ?
 							(size_t)fmt_skb->len :
 							(size_t)16, false);
+#endif
 			}
 
 			dev_kfree_skb_any(fmt_skb);
@@ -1321,8 +1324,10 @@ static int if_hsi_write(struct if_hsi_channel *channel, u32 *data,
 		mipi_err("ch=%d, hsi_write_done timeout : %d\n",
 					channel->channel_id, size);
 
+#if DEBUG_PRINT
 		print_hex_dump_bytes("[HSI]", DUMP_PREFIX_OFFSET,
 						channel->tx_data, size);
+#endif
 
 		hsi_write_cancel(channel->dev);
 
@@ -1541,8 +1546,10 @@ static void if_hsi_read_done(struct hsi_device *dev, unsigned int size)
 			if (ret)
 				mipi_err("send_cmd fail=%d\n", ret);
 
+#if DEBUG_PRINT
 			print_hex_dump_bytes("[HSI]", DUMP_PREFIX_OFFSET,
 					channel->rx_data, channel->packet_size);
+#endif
 
 			/* to clean the all wrong packet */
 			channel->packet_size = 0;
@@ -1550,6 +1557,7 @@ static void if_hsi_read_done(struct hsi_device *dev, unsigned int size)
 			return;
 		}
 
+#if DEBUG_PRINT
 		if ((iod->format == IPC_FMT) ||
 					(iod->format == IPC_RFS))
 			print_hex_dump(KERN_DEBUG,
@@ -1561,6 +1569,7 @@ static void if_hsi_read_done(struct hsi_device *dev, unsigned int size)
 					channel->packet_size <= 16 ?
 					(size_t)channel->packet_size :
 					(size_t)16, false);
+#endif
 
 		channel->packet_size = 0;
 
