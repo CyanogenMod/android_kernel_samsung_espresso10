8f839ecd4aab467dbe471b45537fdf7461245239
andi34 <skate4life@gmx.de>
2014-06-23 23:14:39 +0200

    Upstream 3.0.38 -> 3.0.39
    https://git.kernel.org/cgit/linux/kernel/git/stable/linux-stable.git/log/?h=linux-3.0.y
    
    Allready in before:
    mm/vmstat.c: cache align vm_stat
    https://git.kernel.org/cgit/linux/kernel/git/stable/linux-stable.git/commit/?h=linux-3.0.y&id=9116bc4fb2f96d4a3190932bf17088174da04401
    (Note: we ifdefed the changes already)
    
    vmscan: promote shared file mapped pages
    https://git.kernel.org/cgit/linux/kernel/git/stable/linux-stable.git/commit/?h=linux-3.0.y&id=03722816ec065d8c7a9306fc2d601251bc0c623e
    (Note: we ifdefed the changes already)
    
    vmscan: activate executable pages after first usage
    https://git.kernel.org/cgit/linux/kernel/git/stable/linux-stable.git/commit/?h=linux-3.0.y&id=4391b5f49e28bdb78ddf67495abb2f767474216d
    
    Conflicting patches not applyed:
    mm: migration: clean up unmap_and_move()
    https://git.kernel.org/cgit/linux/kernel/git/stable/linux-stable.git/commit/?h=linux-3.0.y&id=331fae62e66ac4209f23df0df66999932e513fff
    
    mm: compaction: determine if dirty pages can be migrated without blocking within ->migratepage
    https://git.kernel.org/cgit/linux/kernel/git/stable/linux-stable.git/commit/?h=linux-3.0.y&id=397d9c507ff1c9c5afc80c80ee245c2455d6a1db
    
    mm: compaction: introduce sync-light migration for use by compaction
    https://git.kernel.org/cgit/linux/kernel/git/stable/linux-stable.git/commit/?h=linux-3.0.y&id=f869774c37710ef2b773d167d184b9936988d07f
    
    Change-Id: Ic19c4278637fecbe08bf3f3a28365bd2e4af0b10

--- mm/compaction.c
+++ mm/compaction.c
@@ -227,6 +227,7 @@ isolate_migratepages_range(struct zone *zone, struct compact_control *cc,
 	unsigned long last_pageblock_nr = 0, pageblock_nr;
 	unsigned long nr_scanned = 0, nr_isolated = 0;
 	struct list_head *migratelist = &cc->migratepages;
+	isolate_mode_t mode = ISOLATE_ACTIVE|ISOLATE_INACTIVE;
 
 	/*
 	 * Ensure that there are not too many pages isolated from the LRU
@@ -302,8 +303,11 @@ isolate_migratepages_range(struct zone *zone, struct compact_control *cc,
 			continue;
 		}
 
+		if (!cc->sync)
+			mode |= ISOLATE_ASYNC_MIGRATE;
+
 		/* Try isolate the page */
-		if (__isolate_lru_page(page, ISOLATE_BOTH, 0) != 0)
+		if (__isolate_lru_page(page, mode, 0) != 0)
 			continue;
 
 		VM_BUG_ON(PageTransCompound(page));
