84924c957a1d364f742957383774f7ea66a32713
andi34 <skate4life@gmx.de>
2014-06-23 21:47:49 +0200

    Upstream 3.0.36 -> 3.0.37
    https://git.kernel.org/cgit/linux/kernel/git/stable/linux-stable.git/log/?h=linux-3.0.y
    
    Allready in before:
    ARM: fix rcu stalls on SMP platforms
    https://git.kernel.org/cgit/linux/kernel/git/stable/linux-stable.git/commit/?h=linux-3.0.y&id=d1877392b4dce5d4c74d329777980d2a3ccede99
    
    mm, thp: abort compaction if migration page cannot be charged to memcg
    https://git.kernel.org/cgit/linux/kernel/git/stable/linux-stable.git/commit/?h=linux-3.0.y&id=c58c52e0f44d2883ddc31ac021b88a121b332982
    
    mm: Hold a file reference in madvise_remove
    https://git.kernel.org/cgit/linux/kernel/git/stable/linux-stable.git/commit/?h=linux-3.0.y&id=e12fcd38abe8a869cbabd77724008f1cf812a3e7
    
    Change-Id: I2502d92cde53e9d5ab2a9e41ec8435027e25ae0b

--- mm/madvise.c
+++ mm/madvise.c
@@ -221,10 +221,10 @@ static long madvise_remove(struct vm_area_struct *vma,
 			+ ((loff_t)vma->vm_pgoff << PAGE_SHIFT);
 
 	/*
-	 * vmtruncate_range may need to take i_mutex.  We need to
-	 * explicitly grab a reference because the vma (and hence the
-	 * vma's reference to the file) can go away as soon as we drop
-	 * mmap_sem.
+	 * vmtruncate_range may need to take i_mutex and i_alloc_sem.
+	 * We need to explicitly grab a reference because the vma (and
+	 * hence the vma's reference to the file) can go away as soon as
+	 * we drop mmap_sem.
 	 */
 	get_file(f);
 	up_read(&current->mm->mmap_sem);
