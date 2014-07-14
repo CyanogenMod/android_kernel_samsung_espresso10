08fcc54908b6447fb96b45e1200c6986df80180e
andi34 <skate4life@gmx.de>
2014-07-14 02:17:45 +0200

    3.0.31 -> 3.0.101 Upstream from git.kernel.org
    https://git.kernel.org/cgit/linux/kernel/git/stable/linux-stable.git/log/?h=linux-3.0.y
    
    Not applyed Upstream 3.0.38-49:
    mm: migration: clean up unmap_and_move()
    https://git.kernel.org/cgit/linux/kernel/git/stable/linux-stable.git/commit/?h=linux-3.0.y&id=331fae62e66ac4209f23df0df66999932e513fff
    
    mm: compaction: determine if dirty pages can be migrated without blocking within ->migratepage
    https://git.kernel.org/cgit/linux/kernel/git/stable/linux-stable.git/commit/?h=linux-3.0.y&id=397d9c507ff1c9c5afc80c80ee245c2455d6a1db
    
    mm: compaction: introduce sync-light migration for use by compaction
    https://git.kernel.org/cgit/linux/kernel/git/stable/linux-stable.git/commit/?h=linux-3.0.y&id=f869774c37710ef2b773d167d184b9936988d07f
    
    Not applyed Upstream 3.0.39-40:
    ARM: OMAP2+: OPP: Fix to ensure check of right oppdef after bad one
    https://git.kernel.org/cgit/linux/kernel/git/stable/linux-stable.git/commit/?h=linux-3.0.y&id=c3d6a03a5702fb0971109d235dd3c74a5bd08248
    
    ASoC: wm8994: Ensure there are enough BCLKs for four channels
    https://git.kernel.org/cgit/linux/kernel/git/stable/linux-stable.git/commit/?h=linux-3.0.y&id=3b6ae1807d29f8cacd63fddf513110f9308b4c8e
    (this breaks stereo audio on Galaxy Tab 2)
    
    Not applyed Upstream 3.0.67-68:
    cgroup: fix exit() vs rmdir() race
    https://git.kernel.org/cgit/linux/kernel/git/stable/linux-stable.git/commit/?h=linux-3.0.y&id=cc0e3e13b0a90e5ff42d5b134939eacf5e7e497c
    
    Not applyed Upstream 3.0.91-92:
    ARM: 7080/1: l2x0: make sure I&D are not locked down on init
    https://git.kernel.org/cgit/linux/kernel/git/stable/linux-stable.git/commit/?h=linux-3.0.y&id=0e69b54fa8b48e3cdc1a78f77beab3af763a33a1
    
    Change-Id: Ifb7fa4b149e3feaae0a848c909673284151d02e1

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -355,13 +355,12 @@ static int mmc_read_ext_csd(struct mmc_card *card, u8 *ext_csd)
 	card->ext_csd.raw_trim_mult =
 		ext_csd[EXT_CSD_TRIM_MULT];
 	if (card->ext_csd.rev >= 4) {
+	card->ext_csd.raw_partition_support = ext_csd[EXT_CSD_PARTITION_SUPPORT];
 		/*
 		 * Enhanced area feature support -- check whether the eMMC
 		 * card has the Enhanced area enabled.  If so, export enhanced
 		 * area offset and size to user by adding sysfs interface.
 		 */
-		card->ext_csd.raw_partition_support =
-				ext_csd[EXT_CSD_PARTITION_SUPPORT];
 		if ((ext_csd[EXT_CSD_PARTITION_SUPPORT] & 0x2) &&
 		    (ext_csd[EXT_CSD_PARTITION_ATTRIBUTE] & 0x1)) {
 			u8 hc_erase_grp_sz =
