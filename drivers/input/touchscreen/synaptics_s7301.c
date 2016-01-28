5409c71815048a34ce17acba67804e2f9a7aa317
Ziyan <jaraidaniel@gmail.com>
2016-01-28 13:56:45 +0100

    defconfigs: build drivers for all variants
    Change-Id: I23ac51effba7cf97581217bc1d80a048959c066b

--- drivers/input/touchscreen/synaptics_s7301.c
+++ drivers/input/touchscreen/synaptics_s7301.c
@@ -120,25 +120,6 @@ static int ts_read_reg_data(const struct i2c_client *client, u8 address,
 	return 1;
 }
 
-static int ts_write_reg_data(const struct i2c_client *client, u8 address,
-			u8 *buf, u8 size)
-{
-	int ret = 0;
-
-	if (size > 32) {
-		pr_err("tsp: %s: data size: %d, SMBus allows at most 32 bytes.",
-								__func__, size);
-		return -1;
-	}
-
-	ret = i2c_smbus_write_i2c_block_data(client, address, size, buf);
-	if (ret < 0) {
-		pr_err("tsp: %s: i2c write failed. %d", __func__, ret);
-		return ret;
-	}
-	return 1;
-}
-
 static void set_ta_mode(int *ta_state)
 {
 	struct sec_ts_platform_data *platform_data =
@@ -738,7 +719,7 @@ static void run_tx_to_gnd_read(void *device_data)
 	return;
 }
 
-struct tsp_cmd tsp_cmds[] = {
+struct tsp_cmd tsp_cmds_synaptics[] = {
 	{TSP_CMD("fw_update", fw_update),},
 	{TSP_CMD("get_fw_ver_bin", get_fw_ver_bin),},
 	{TSP_CMD("get_fw_ver_ic", get_fw_ver_ic),},
@@ -1232,8 +1213,8 @@ static int __devinit ts_probe(struct i2c_client *client,
 	}
 
 	INIT_LIST_HEAD(&factory_data->cmd_list_head);
-	for (i = 0; i < ARRAY_SIZE(tsp_cmds); i++)
-		list_add_tail(&tsp_cmds[i].list, &factory_data->cmd_list_head);
+	for (i = 0; i < ARRAY_SIZE(tsp_cmds_synaptics); i++)
+		list_add_tail(&tsp_cmds_synaptics[i].list, &factory_data->cmd_list_head);
 
 	mutex_init(&factory_data->cmd_lock);
 	factory_data->cmd_is_running = false;
