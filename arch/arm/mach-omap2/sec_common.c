5ebf259f1ec95257d7cea9e8694b66976571cc3d
Ziyan <jaraidaniel@gmail.com>
2016-03-28 03:46:48 +0200

    espresso: move reboot notifier to board files
    Change-Id: I5d807ad7b2cf488227da80cb53102e99e461298d

--- arch/arm/mach-omap2/sec_common.c
+++ arch/arm/mach-omap2/sec_common.c
@@ -18,7 +18,6 @@
 #include <linux/kallsyms.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/reboot.h>
 #include <linux/sort.h>
 
 #include <mach/hardware.h>
@@ -31,25 +30,8 @@
 #include "omap_muxtbl.h"
 #include "sec_common.h"
 
-#if defined(CONFIG_ARCH_OMAP3)
-#define SEC_REBOOT_MODE_ADDR		(OMAP343X_CTRL_BASE + 0x0918)
-#define SEC_REBOOT_FLAG_ADDR		(OMAP343X_CTRL_BASE + 0x09C4)
-#define SEC_REBOOT_CMD_ADDR		NULL
-#elif defined(CONFIG_ARCH_OMAP4)
-#define OMAP_SW_BOOT_CFG_ADDR		0x4A326FF8
-#define SEC_REBOOT_MODE_ADDR		(OMAP_SW_BOOT_CFG_ADDR)
-#define SEC_REBOOT_FLAG_ADDR		(OMAP_SW_BOOT_CFG_ADDR - 0x04)
-/* -0x08/-0x0C are reserved for debug */
-#define SEC_REBOOT_CMD_ADDR		(OMAP_SW_BOOT_CFG_ADDR - 0x10)
-#else
-#error "unsupported mach-type for OMAP-Samsung"
-#endif
-
 struct class *sec_class;
 EXPORT_SYMBOL(sec_class);
-
-void (*__arch_reset)(char, const char *);
-static void sec_common_arch_reset(char mode, const char *cmd);
 
 char sec_androidboot_mode[16];
 EXPORT_SYMBOL(sec_androidboot_mode);
@@ -61,76 +43,6 @@ static __init int setup_androidboot_mode(char *opt)
 }
 
 __setup("androidboot.mode=", setup_androidboot_mode);
-
-struct sec_reboot_code {
-	char *cmd;
-	int mode;
-};
-
-static int __sec_common_cmp_reboot_cmd(const void *code0, const void *code1)
-{
-	struct sec_reboot_code *r_code0 = (struct sec_reboot_code *)code0;
-	struct sec_reboot_code *r_code1 = (struct sec_reboot_code *)code1;
-
-	return (strlen(r_code0->cmd) > strlen(r_code1->cmd)) ? -1 : 1;
-}
-
-static int __sec_common_reboot_call(struct notifier_block *this,
-				    unsigned long code, void *cmd)
-{
-	int mode = REBOOT_MODE_NONE;
-	unsigned int value;
-	size_t cmd_len;
-
-	struct sec_reboot_code reboot_tbl[] = {
-		{"arm11_fota", REBOOT_MODE_PREFIX | REBOOT_MODE_ARM11_FOTA},
-		{"arm9_fota", REBOOT_MODE_PREFIX | REBOOT_MODE_ARM9_FOTA},
-		{"recovery", REBOOT_MODE_PREFIX | REBOOT_MODE_RECOVERY},
-		{"cp_crash", REBOOT_MODE_PREFIX | REBOOT_MODE_CP_CRASH},
-		{"fota", REBOOT_MODE_PREFIX | REBOOT_MODE_ARM11_FOTA},
-		{"fota_bl", REBOOT_MODE_PREFIX | REBOOT_MODE_FOTA_BL},
-		{"debug", REBOOT_SET_PREFIX | REBOOT_SET_DEBUG},
-		{"swsel", REBOOT_SET_PREFIX | REBOOT_SET_SWSEL},
-		{"sud", REBOOT_SET_PREFIX | REBOOT_SET_SUD},
-	};
-	size_t i, n;
-
-	sort(reboot_tbl, ARRAY_SIZE(reboot_tbl),
-	     sizeof(struct sec_reboot_code),
-	     __sec_common_cmp_reboot_cmd, NULL);
-
-	if ((code == SYS_RESTART) && cmd) {
-		n = ARRAY_SIZE(reboot_tbl);
-		for (i = 0; i < n; i++) {
-			cmd_len = strlen(reboot_tbl[i].cmd);
-			if (!strncmp((char *)cmd,
-				     reboot_tbl[i].cmd, cmd_len)) {
-				mode = reboot_tbl[i].mode;
-				break;
-			}
-		}
-
-		if (mode == (REBOOT_SET_PREFIX | REBOOT_SET_DEBUG) ||
-		    mode == (REBOOT_SET_PREFIX | REBOOT_SET_SWSEL) ||
-		    mode == (REBOOT_SET_PREFIX | REBOOT_SET_SUD)) {
-			if (!kstrtouint((char *)cmd + cmd_len, 0, &value))
-				mode |= value;
-			else
-				mode = REBOOT_MODE_NONE;
-		}
-		pr_info("(%s): mode = 0x%x, cmd = %s\n",
-			__func__, mode, (char *)cmd);
-	}
-
-	if (SEC_REBOOT_CMD_ADDR)
-		omap_writel(mode, SEC_REBOOT_CMD_ADDR);
-
-	return NOTIFY_DONE;
-}				/* end fn __sec_common_reboot_call */
-
-static struct notifier_block __sec_common_reboot_notifier = {
-	.notifier_call = __sec_common_reboot_call,
-};
 
 /*
  * Store a handy board information string which we can use elsewhere like
@@ -162,10 +74,6 @@ int __init sec_common_init_early(void)
 {
 	sec_common_set_panic_string();
 
-	/* replacing arch_reset */
-	__arch_reset = arch_reset;
-	arch_reset = sec_common_arch_reset;
-
 	return 0;
 }				/* end fn sec_common_init_early */
 
@@ -189,93 +97,3 @@ int __init sec_common_init(void)
 
 	return 0;
 }				/* end fn sec_common_init */
-
-int __init sec_common_init_post(void)
-{
-	register_reboot_notifier(&__sec_common_reboot_notifier);
-
-	return 0;
-}				/* end fn sec_common_init_post */
-
-struct sec_reboot_mode {
-	char *cmd;
-	char mode;
-};
-
-static char __sec_common_convert_reboot_mode(char mode,  const char *cmd)
-{
-	char new_mode = mode;
-	struct sec_reboot_mode mode_tbl[] = {
-		{"arm11_fota", 'f'},
-		{"arm9_fota", 'f'},
-		{"recovery", 'r'},
-        {"bootloader", 'b'},
-		{"download", 'd'},
-		{"cp_crash", 'C'}
-	};
-	size_t i, n;
-	if (cmd == NULL)
-		goto __return;
-	n = ARRAY_SIZE(mode_tbl);
-	for (i = 0; i < n; i++) {
-		if (!strcmp(cmd, mode_tbl[i].cmd)) {
-			new_mode = mode_tbl[i].mode;
-			goto __return;
-		}
-	}
-
-__return:
-	return new_mode;
-}
-
-static int sec_common_update_reboot_reason(char mode, const char *cmd)
-{
-	u32 scpad = 0;
-	const u32 scpad_addr = SEC_REBOOT_MODE_ADDR;
-	u32 reason = REBOOTMODE_NORMAL;
-	char *rebootflag = "RESET";
-
-#if defined(CONFIG_ARCH_OMAP3)
-	scpad = omap_readl(scpad_addr);
-#endif /* CONFIG_ARCH_OMAP3 */
-
-	/* for the compatibility with LSI chip-set based products */
-	mode = __sec_common_convert_reboot_mode(mode, cmd);
-
-	switch (mode) {
-	case 'r':		/* reboot mode = recovery */
-		reason = REBOOTMODE_RECOVERY;
-		break;
-	case 'f':		/* reboot mode = fota */
-		reason = REBOOTMODE_FOTA;
-		break;
-	case 't':		/* reboot mode = shutdown with TA */
-	case 'u':		/* reboot mode = shutdown with USB */
-	case 'j':		/* reboot mode = shutdown with JIG */
-		reason = REBOOTMODE_SHUTDOWN;
-		rebootflag = "POFF";
-		break;
-	case 'b':		/* reboot mode = bootloader */
-		reason = REBOOTMODE_DOWNLOAD;
-		break;
-	case 'd':		/* reboot mode = download */
-		reason = REBOOTMODE_DOWNLOAD;
-		break;
-	default:		/* reboot mode = normal */
-		reason = REBOOTMODE_NORMAL;
-		break;
-	}
-
-	omap_writel(scpad | reason, scpad_addr);
-	omap_writel(*(u32 *)rebootflag, SEC_REBOOT_FLAG_ADDR);
-
-	return (int)mode;
-}				/* sec_common_update_reboot_reason */
-
-static void sec_common_arch_reset(char mode, const char *cmd)
-{
-	mode = sec_common_update_reboot_reason(mode, cmd);
-
-	if (__arch_reset)
-		__arch_reset(mode, cmd);
-}
--- arch/arm/mach-omap2/sec_common.h
+++ arch/arm/mach-omap2/sec_common.h
@@ -15,40 +15,14 @@
 #ifndef __SEC_COMMON_H__
 #define __SEC_COMMON_H__
 
-#define REBOOTMODE_NORMAL		(1 << 0)
-#define REBOOTMODE_RECOVERY		(1 << 1)
-#define REBOOTMODE_FOTA			(1 << 2)
-#define REBOOTMODE_SHUTDOWN		(1 << 4)
-#define REBOOTMODE_DOWNLOAD             (1 << 5)
-
-/* REBOOT_MODE */
-#define REBOOT_MODE_PREFIX		0x12345670
-#define REBOOT_MODE_NONE		0
-#define REBOOT_MODE_FACTORYTEST		1
-#define REBOOT_MODE_RECOVERY		2
-#define REBOOT_MODE_ARM11_FOTA		3
-#define REBOOT_MODE_DOWNLOAD		4
-#define REBOOT_MODE_CHARGING		5
-#define REBOOT_MODE_ARM9_FOTA		6
-#define REBOOT_MODE_CP_CRASH		7
-#define REBOOT_MODE_FOTA_BL		8
-
-#define	REBOOT_SET_PREFIX		0xabc00000
-#define	REBOOT_SET_DEBUG		0x000d0000
-#define	REBOOT_SET_SWSEL		0x000e0000
-#define REBOOT_SET_SUD			0x000f0000
-
 int sec_common_init_early(void);
 
 int sec_common_init(void);
-
-int sec_common_init_post(void);
 
 extern struct class *sec_class;
 
 extern unsigned int system_rev;
 
-extern u32 sec_bootmode;
 extern char sec_androidboot_mode[16];
 
 #endif /* __SEC_COMMON_H__ */
