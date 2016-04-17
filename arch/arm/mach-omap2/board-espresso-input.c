9d097557afafef8b52699eb3d9ad7331808a3a45
Andreas Blaesius <skate4life@gmx.de>
2016-04-17 20:48:56 +0200

    espresso: mms136: get rid of CONFIG_TOUCH_DVFS
    Change-Id: I8ee62f7fa68f893dc4ff6043fb281c096c9f8bba

--- arch/arm/mach-omap2/board-espresso-input.c
+++ arch/arm/mach-omap2/board-espresso-input.c
@@ -390,23 +390,6 @@ void omap4_espresso_tsp_ta_detect(int cable_type)
 	return;
 }
 
-static void espresso_set_dvfs(bool on)
-{
-#ifdef CONFIG_TOUCH_DVFS
-	if (!board_is_espresso10()) {
-		static bool is_on;
-
-		if (on && !is_on) {
-			omap_cpufreq_min_limit(DVFS_LOCK_ID_TSP, CONFIG_TOUCH_DVFS);
-			is_on = true;
-		} else if (!on && is_on) {
-			omap_cpufreq_min_limit_free(DVFS_LOCK_ID_TSP);
-			is_on = false;
-		}
-	}
-#endif
-}
-
 void __init omap4_espresso_input_init(void)
 {
 	espresso_gpio_keypad_gpio_init();
@@ -425,8 +408,4 @@ void __init omap4_espresso_input_init(void)
 	espresso_create_sec_key_dev();
 
 	platform_device_register(&espresso_gpio_keypad_device);
-
-	if (!board_is_espresso10()) {
-		espresso_ts_pdata.set_dvfs = espresso_set_dvfs;
-	}
 }
